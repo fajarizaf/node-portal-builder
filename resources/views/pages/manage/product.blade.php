f36ef68a8eef04fb1a5427305ffea5d1a4f85dd8
Fajar Riza Fauzi <fajarizaf@gmail.com>
2024-10-15 20:57:15 +0700

    update

--- resources/views/pages/manage/product.blade.php
+++ resources/views/pages/manage/product.blade.php
@@ -139,7 +139,9 @@
                 <div class="col-auto">
                     <div class="card-body">
                         <div class="img-result rounded" style="overflow:hidden;">
-                            <img style="width:120px" src='{{asset('storage/uploads/'.ProdukHelper::get_url_photo($row->id))}}' />
+                            <img style="width:120px" src='{{Storage::url("uploads/".ProdukHelper::get_url_photo($row->id))}}' />
+
+
 
                         </div>
                     </div>
