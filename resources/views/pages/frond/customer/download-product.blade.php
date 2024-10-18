ec84762376c2c78f1c359b11bb0f28d3a8a8ca0d
Fajar Riza Fauzi <fajarizaf@gmail.com>
2024-10-18 11:45:04 +0700

    update download fitur

--- resources/views/pages/frond/customer/download-product.blade.php
+++ resources/views/pages/frond/customer/download-product.blade.php
@@ -50,26 +50,38 @@ $color = StoreHelper::Display_color($site_id);
 
 <div class="card card-md">
 
-    <div class="form-selectgroup-label d-flex align-items-center p-3" style="justify-content:space-between;margin-bottom:10px;margin-top:10px;">
+    <div class="card-body text-center py-4 p-sm-5">
 
-        <div>
-            <small class="form-hint">
-                Nomor Pesanan :
-            </small>
+        <div class="form-selectgroup-label d-flex align-items-center p-3" style="justify-content:space-between;margin-bottom:10px;margin-top:10px;">
 
-            <b id="myText">ODR-{{$product->order_id}}</b>
+            <div>
+                <small class="form-hint">
+                    Nomor Pemesanan :
+                </small>
+
+                <b id="myText">ODR-{{$product->order_id}}</b>
+            </div>
+            <div>
+                <small class="form-hint">
+                    Tanggal Pemesanan :
+                </small>
+
+                <b id="myText">{{\Carbon\Carbon::parse($product->invoices_date)->format('d M Y - H:i:s')}}</b>
+
+            </div>
+
+            <div onclick="copyContent()">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-library">
+                    <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                    <path d="M7 3m0 2.667a2.667 2.667 0 0 1 2.667 -2.667h8.666a2.667 2.667 0 0 1 2.667 2.667v8.666a2.667 2.667 0 0 1 -2.667 2.667h-8.666a2.667 2.667 0 0 1 -2.667 -2.667z" />
+                    <path d="M4.012 7.26a2.005 2.005 0 0 0 -1.012 1.737v10c0 1.1 .9 2 2 2h10c.75 0 1.158 -.385 1.5 -1" />
+                    <path d="M11 7h5" />
+                    <path d="M11 10h6" />
+                    <path d="M11 13h3" />
+                </svg>
+            </div>
+
         </div>
-        <div onclick="copyContent()">
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-library">
-                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                <path d="M7 3m0 2.667a2.667 2.667 0 0 1 2.667 -2.667h8.666a2.667 2.667 0 0 1 2.667 2.667v8.666a2.667 2.667 0 0 1 -2.667 2.667h-8.666a2.667 2.667 0 0 1 -2.667 -2.667z" />
-                <path d="M4.012 7.26a2.005 2.005 0 0 0 -1.012 1.737v10c0 1.1 .9 2 2 2h10c.75 0 1.158 -.385 1.5 -1" />
-                <path d="M11 7h5" />
-                <path d="M11 10h6" />
-                <path d="M11 13h3" />
-            </svg>
-        </div>
-
     </div>
 
 
